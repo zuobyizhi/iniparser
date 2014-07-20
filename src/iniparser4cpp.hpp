429d2ef8e94442fb4e85d507671e39ccdcf7687a
zuo <zuobyige@sina.cn>
2014-07-20 08:00:33 +0800

    repair a bug
    repair a bug

--- src/iniparser4cpp.hpp
+++ src/iniparser4cpp.hpp
@@ -22,56 +22,72 @@ class iniparser4cpp : public noncopyable
 {
 public:
     iniparser4cpp(const std::string& inifile)
+    : ini(nullptr)
     {
-        assert(!inifile.empty());
-        ini = iniparser_load(inifile.c_str());
+        reset(inifile);
     }
     ~iniparser4cpp()
     {
         iniparser_freedict(ini);
     }
 
-    int getnsec(void)
+    inline bool invalid(void) const
+    {
+        return nullptr==ini;
+    }
+
+    void reset(const std::string& inifile)
+    {
+        assert(!inifile.empty());
+        if (!invalid())
+        {
+            iniparser_freedict(ini);
+            assert(invalid());
+        }
+        ini = iniparser_load(inifile.c_str());
+    }
+
+    inline int getnsec(void)
     {
         return iniparser_getnsec(ini);
     }
 
-    std::string iniparser_getsecname(int n)
+    inline std::string getsecname(int n)
     {
         return std::string(iniparser_getsecname(ini, n));
     }
 
-    std::string getstring(const char * key, char * def=nullptr)
+    inline std::string getstring(const char * key, char * def=nullptr)
     {
         return std::string(iniparser_getstring(ini, key, def));
     }
 
-    int getint(const char * key, int notfound=0)
+    inline int getint(const char * key, int notfound=0)
     {
         return iniparser_getint(ini, key, notfound);
     }
 
-    double getdouble(const char * key, double notfound=0.0)
+    inline double getdouble(const char * key, double notfound=0.0)
     {
         return iniparser_getdouble(ini, key, notfound);
     }
 
-    bool getboolean(const char * key, bool notfound=false)
+    inline bool getboolean(const char * key, bool notfound=false)
     {
         return iniparser_getboolean(ini, key, notfound);
     }
 
-    bool set(const char * entry, const char * val)
+    inline bool set(const char * entry, const char * val)
     {
         return 0==iniparser_set(ini, entry, val);
     }
 
-    void unset(dictionary * ini, const char * entry)
+    inline void unset(dictionary * ini, const char * entry)
     {
         iniparser_unset(ini, entry);
     }
 
-    bool find_entry(const char * entry)
+    inline bool find_entry(const char * entry)
     {
         return 1==iniparser_find_entry(ini, entry);
     }
